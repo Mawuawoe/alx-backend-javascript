589802158be67de404b121590afb25b66a7b14d1
Mawuawoe <desmonddzakago@gmail.com>
2024-10-05 21:19:02 +0000

    task_3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,17 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([
+    uploadPhoto(),
+    createUser(),
+  ])
+    .then((resultArray) => {
+      const uploadResult = resultArray[0]; // Result from uploadPhoto
+      const userResult = resultArray[1]; // Result from createUser
+
+      console.log(uploadResult.body, userResult.firstName, userResult.lastName);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
