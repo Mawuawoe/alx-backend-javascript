6f75c4fb2ed272a420541354d77179a56a64f991
Mawuawoe <desmonddzakago@gmail.com>
2024-10-06 14:53:28 +0000

    task_6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,24 @@
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+
+// uploadPhoto is a function that returns a promise that rejects
+// signUpUser return a promise that resolves
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    uploadPhoto(fileName),
+    signUpUser(firstName, lastName),
+  ])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      }
+      return {
+        status: result.status,
+        value: result.reason,
+      };
+    }));
+}
