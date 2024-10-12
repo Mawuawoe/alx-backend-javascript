7d2e9315ec2f8c717f2b9e4a543d9d7ef8bb2fc2
Mawuawoe <desmonddzakago@gmail.com>
2024-10-12 13:11:49 +0000

    task_0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 function getListStudents() {
   return [
-    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
-    { firstName: 'James', id: 2, location: 'Columbia' },
-    { firstName: 'Serana', id: 5, location: 'San Francisco' },
+    { firstName: "Guillaume", id: 1, location: 'San Francisco' },
+    { firstName: "James", id: 2, location: 'Columbia' },
+    { firstName: "Serana", id: 5, location: 'San Francisco' },
   ];
 }
 
