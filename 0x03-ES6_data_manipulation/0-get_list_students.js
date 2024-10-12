e0b87099a33c9562db08fb6b8813d8363c6368cc
Mawuawoe <desmonddzakago@gmail.com>
2024-10-12 15:07:00 +0000

    task_1

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,12 +2,10 @@
  * Returns a list of students.
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
-function getListStudents() {
+export default function getListStudents() {
   return [
     { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
     { firstName: 'James', id: 2, location: 'Columbia' },
     { firstName: 'Serana', id: 5, location: 'San Francisco' },
   ];
 }
-
-module.exports = getListStudents;
---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,5 @@
+export default function getListStudentIds(arrObj) {
+  if (Array.isArray(arrObj)) return arrObj.map((obj) => obj.id);
+
+  return [];
+}
