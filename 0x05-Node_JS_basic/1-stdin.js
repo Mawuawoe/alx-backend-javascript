a8ec6e493fe0e7a0449b3856087ef648b76cec01
Mawuawoe <desmonddzakago@gmail.com>
2024-11-28 21:17:55 +0000

    task_1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,24 +1,16 @@
-// Display a welcome message
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Listen for user input
 if (process.stdin.isTTY) {
-  process.stdin.on('data', (input) => {
-    // remove extra whitespace or newlines
-    const name = input.trim();
-    console.log(`Your name is: ${name}`);
-
-    // Exit the program after displaying the input
-    process.exit(0);
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
   });
 } else {
-  process.stdin.on('data', (input) => {
-    // remove extra whitespace or newlines
-    const name = input.trim();
-    console.log(`Your name is: ${name}`);
-
-    // Exit the program after displaying the input
-    console.log('This important software is now closing');
-    process.exit(0);
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
   });
 }
